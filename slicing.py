2461afe6a9a6f45bc26aeb4b6b40dd37da08fa88
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-04-01 14:35:25 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,34 @@
+# a="python"
+# print(a[ : : ]) #python
+# print(a[ 0 : : 1]) #python
+# print(a[ : :2]) #pto
+# print(a[ : :3]) #ph
+# print(a[ : :4]) #p0
+# print(a[ : :5]) #pn
+# print(a[ : :6]) #p
+# print(a[ : : -1]) #nothyp
+# print(a[ : : -2]) #nhy
+# print(a[ : : -3]) #nt
+# print(a[ : : -4]) #ny
+# print(a[ : : -5]) #np
+# print(a[ : : -6]) #n
+
+
+# a="koteswari"
+# print(a[1: : ]) #oteswari
+# print(a[2: : ]) #teswari
+# print(a[3: : ]) #eswari
+# print(a[4: : ]) #swari
+# print(a[5: : ]) #wari
+# print(a[6: : ]) #ari
+# print(a[7: : ]) #ri
+# print(a[8: : ]) #r
+# print(a[9: : ]) #blankline
+
+b="venkat"
+print(b[1: 6: ]) #enkat
+print(b[2: 5: ]) #nka
+print(b[3: 4: ]) #k
+print(b[4: 3: ]) #blankline
+print(b[5: 2: ]) #blankline
+print(b[6: 1: ]) #blankline
