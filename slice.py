f845b17c1512034e3c4f3cbe561666f0cd95eded
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-04-01 14:28:05 +0530

    Add files via upload

---
+++ slice.py
@@ -0,0 +1,61 @@
+# print(len("python")) #6
+# a="python"
+# print(a,type(a)) #python str
+# print(a[0]) #p
+# print("python"[0]) #p
+# print("python"[1]) #y
+# print("python"[2]) #t
+# print("python"[3]) #h
+# print("python"[4]) #o
+# print("python"[5]) #n
+# print("python"[6]) #indexerror
+# v="department"
+# print(v[3]+v[2]+v[3]+v[4]+v[5]+v[6]+v[7]+v[8]+v[9]) #apartment
+# print("python"[-1]) #n
+# print("python"[-2]) #o
+# print("python"[-3]) #h
+# print("python"[-4]) #t
+# print("python"[-5]) #y 
+# print("python"[-6]) #p
+# print("python"[-7]) #indexerror
+# w="python"
+# print(w[:]) #python
+# print(w[0:]) #python
+# print(w[0:6]) #python
+# print(w[0:1]) #p
+# print(w[0:2]) #py
+# print(w[0:3]) #pyt
+# print(w[0:4]) #pyth
+# print(w[0:5]) #pytho
+# print(w[0:6]) #python
+# print(w[0:7]) #python
+# print(w[0:99]) #python
+# print(w[0:0]) #blankline
+# print(w[3:2]) #blankline
+# print(w[5:3]) #blankline
+# print(w[0:-1]) #pytho
+# print(w[0:-2]) #pyth
+# print(w[0:-3]) #pyt
+# print(w[0:-4]) #py
+# print(w[0:-5]) #p
+# print(w[0:-6]) #blankline
+# print(w[0:-7]) #blankline
+
+# r="python"
+# print(r[2:5]) #th
+# # print(r[3:4]) #h
+# print(r[5:2]) #blankline
+# # print(r[5:5]) #blankline
+
+# y="python"
+# print(y[-1:-6]) #blankline
+# print(y[2:-5]) #blankline
+# print(y[-6]) #p
+
+# c="python"
+# print(c[ : :]) #python
+# print(c[0: :1])
+# print(c[ : : 2])
+# print(c[ : : 3])
+# print(c[ : : 4])
+
